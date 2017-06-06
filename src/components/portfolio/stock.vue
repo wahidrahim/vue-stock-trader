b005d31ac38bf71faeff1aa3069ef927e50a2ebc
Wahid Rahim <Wahid Rahim>
2017-06-06 11:08:25 -0400

    init

---
+++ src/components/portfolio/stock.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+    <h1>portfolio stock</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+    <h1>stock</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
